19fa9e99278f2bebfc6079bae14b45f103c70914
ravi_teja_n <ravi@quantumgraph.com>
2018-02-23 17:27:03 +0530

    updating after_install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,22 @@
-cd /home/ubuntu/backend/
-virtualenv venv --python=python3.5.2
-pip install -r requirements.txt
-source venv/bin/activate
 
 
-#Db related tasks skip to another file
-python manage.py migrate
 
-mv /home/ubuntu/backend/conf/nginx.conf /etc/nginx/sites-enabled/default
-mv /home/ubuntu/backend/conf/supervisor_backend.conf /etc/supervisor.d/conf.d/
+cp /home/ubuntu/backend/conf/nginx.conf /etc/nginx/sites-enabled/default
+cp /home/ubuntu/backend/conf/supervisor_backend.conf /etc/supervisor/conf.d/
 
 
 
 sudo chown ubuntu:ubuntu -R .
+cd /home/ubuntu/backend/
+rm -rf venv
+virtualenv venv --python=python3.5
+source venv/bin/activate
+pip install -r requirements.txt
+
+#Db related tasks skip to another file
+python manage.py migrate
+python manage.py collectstatic
+
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo service nginx restart
